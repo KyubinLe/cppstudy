3e81a75b7429fabf34b3192a1d712b2c51eb4b05
Kyubin Lee <kyubin@kakao.com>
2022-01-13 16:16:23 +0900

    ㅡ

--- ch01.cpp
+++ ch01.cpp
@@ -30,28 +30,42 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int num1, num2;
-    cout << "입력(정수2개) : ";
-    cin >> num1 >> num2;
-    if(num1 < num2) cout << "가장 큰 수 : "<< num2;
-    else cout << "가장 큰 수 : " << num1;
+// int main()
+// {
+//     int num1, num2;
+//     cout << "입력(정수2개) : ";
+//     cin >> num1 >> num2;
+//     if(num1 < num2) cout << "가장 큰 수 : "<< num2;
+//     else cout << "가장 큰 수 : " << num1;
+//     int n1,n2,n3;
+//     cout << endl << "입력(정수 3개) : ";
+//     cin >> n1 >> n2 >> n3;
+//     if(n1 > n2 && n1 > n3) cout << "가장 큰 수 : " << n1 << endl;
+//     else if(n2 > n1 && n2 > n3) cout << "가장 큰 수 : " << n2 << endl;
+//     else cout << "가장큰 수 : " << n3 << endl;
+//     int ko, en, ma;
+//     cout << "국어점수 입력 : ";
+//     cin >> ko;
+//     cout << "영어점수 입력 : ";
+//     cin >> en;
+//     cout << "수학점수 입력 : ";
+//     cin >> ma;
+//     cout << "성적 총합 : " << ko + en + ma;
+//     cout << "\n성적 평균 : " << (ko + en + ma) / 3;
+// }
 
-    int n1,n2,n3;
-    cout << endl << "입력(정수 3개) : ";
-    cin >> n1 >> n2 >> n3;
-    if(n1 > n2 && n1 > n3) cout << "가장 큰 수 : " << n1 << endl;
-    else if(n2 > n1 && n2 > n3) cout << "가장 큰 수 : " << n2 << endl;
-    else cout << "가장큰 수 : " << n3 << endl;
+// int main(){
+//     char name[100];
+//     char major[100];
+//     cout << "이름 : ";
+//     cin >> name;
+//     cout << "전공 : ";
+//     cin >> major;
+//     cout << "출력\n" << "이름 : " << name << "\t" << "전공 : " << major << endl;
+// }
 
-    int ko, en, ma;
-    cout << "국어점수 입력 : ";
-    cin >> ko;
-    cout << "영어점수 입력 : ";
-    cin >> en;
-    cout << "수학점수 입력 : ";
-    cin >> ma;
-    cout << "성적 총합 : " << ko + en + ma;
-    cout << "\n성적 평균 : " << (ko + en + ma) / 3;
-}
+/* 문제
+사용자로부터 총5개 정수 받아서 합 출력 프로그램, 배열
+사용자로부터 이름 전화번호 문자열 형태로 입력받아 그대로 출력
+단수를 하나 받아 해당 구구단 출력*/
+
