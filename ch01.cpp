41492f3d951a6dcfb745a4362fb90318dcf8cda3
Kyubin Lee <kyubin@kakao.com>
2022-01-13 16:46:05 +0900

    f

--- ch01.cpp
+++ ch01.cpp
@@ -69,3 +69,59 @@ using namespace std;
 사용자로부터 이름 전화번호 문자열 형태로 입력받아 그대로 출력
 단수를 하나 받아 해당 구구단 출력*/
 
+// int sum(int a, int b) { retrun a + b; }
+// int sum(int a, int b int c) { return a + b + c; }
+// double sum(double a, double b) { return a + b; }
+// int main(){
+//     cout << "sum(1,2) : " << sum(1,2) << endl;
+//     cout << "sum(1,2,3)" << sum(1,2,3) << endl;
+//     cout << "sum(1.2,2.3) : " sum(1.2,2.3) << endl;
+// }
+
+// void swap(int* x, int* y)
+// {
+//     int tmp = *x;
+//     *x = *y;
+//     *y = tmp;
+// }
+// int main()
+// {
+//     int n1 = 10, n2 = 20;
+//     cout << "swap 사용 전 n1 n2 : " << n1 << " " << n2 << endl;
+//     swap(&n1, &n2);
+//     cout << "swap 사용 후 n1 n2 : " << n1 << " " << n2 << endl;
+// }
+
+// int main(){
+//     int num = 0;
+
+//     cout << "정수 입력 : ";
+//     cin >> num;
+//     for(int i = 1; i <= 9; i++){
+//         cout << num << " x " << i << " = " << num * i << " " ;
+//     }
+//     return 0;
+// }
+
+// int sum(int a = 10, int b  = 20){
+//     return a + b;
+// }
+// int main(){
+//     cout << "sum() = " << sum() << endl;
+//     cout << "sum(1) = " << sum(1) << endl;
+//     cout << "sum(1, 2) = " << sum(1, 2) << endl;
+// }
+
+// #define Func1(x) x *x
+// inline int Func2(int x) // 매크로 함수를 사용하는 경우도 있지만 보다 복잡한 로직 처리하는경우 inline 예약어를 반환자료형 앞에 붙여서 인라인화처리 또는 일반함수로 치라해 성능효율 업
+// {
+//     return x * x;
+// }
+// int main()
+// {
+//     int result = 0;
+//     int x = 5;
+//     cout << "FUNC(x) = " << Func1(x) << endl;
+//     cout << "FUNC(x) = " << Func2(x + 3) << endl;
+// }
+
