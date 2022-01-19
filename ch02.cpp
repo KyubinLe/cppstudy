daeea10a9bb1deddfbea2f78922b40a47e38f1bf
Kyubin Lee <kyubin@kakao.com>
2022-01-19 15:17:01 +0900

    ㅁ

---
+++ ch02.cpp
@@ -0,0 +1,67 @@
+// #include <iostream>
+// #include <cstdlib>
+// using namespace std;
+
+// // int main(){
+// //     // int num = 1234;
+// //     // int& ref = num;
+// //     // cout << "num : " << num << endl;
+// //     // cout << "ref : " << ref << endl;
+// //     // cout << &num << endl;
+// //     // cout << &ref;
+// //     // return 0;
+// //     // int num = 12;
+// //     // int *ptr = &num;
+// //     // int* (&ref) = ptr;
+// //     // (*ptr)++;
+// //     // (*ref)++;
+// //     // cout<<"num = " << num << endl;
+// //     // return 0;
+// // }
+
+// // int main()
+// // {
+// //     int *ptr1 = (int *)malloc(sizeof(int));
+// //     if (ptr1 == NULL)
+// //     {
+// //         return -1;
+// //     }
+// //     *ptr1 = 100;
+// //     cout << "*ptr1 = " << *ptr1 << endl;
+// //     int *ptr3 = new int;
+// //     *ptr3 = 123;
+// //     cout << "*ptr3 = " << *ptr3 << endl;
+// // // new 사용(배열형)
+// //     int *ptr4 = new int[3];
+// //     ptr4[0] = 123;
+// //     ptr4[1] = 567;
+// //     ptr4[2] = 890;
+// //     cout << "0 : " << ptr4[0] << endl;
+// //     cout << "1 : " << ptr4[1] << endl;
+// //     cout << "2 : " << ptr4[2] << endl;
+
+// //     delete ptr3;
+// //     delete[] ptr4;
+// //     return 0;
+// // }
+
+// class simple{
+//     public:
+//         simple(){
+//             this->n1 = 10;
+//             cout << "기본 생성자 호출" << endl;
+//         }
+// };
+// int main(){
+//     simple* obj1=(simple*)malloc(sieof(simple));
+//     simple* obj2 = new simple;
+
+//     delete obj2;
+// }
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+    
+}
